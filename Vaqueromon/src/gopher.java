375353608962bd2539afbfec041e7904aca6bd34
De1cid3 <leonardo.calendario@gmail.com>
2025-08-20 11:10:58 -0400

    saved gopher changes

--- Vaqueromon/src/gopher.java
+++ Vaqueromon/src/gopher.java
@@ -1,8 +1,10 @@
 public class gopher extends mon {
 
+    private final name = gopher ;
+
     public gopher(String name, int health, int atk, int speed, int defense, Mon_type type) {
         super(name, health, atk, speed, defense, type);
-        //TODO Auto-generated constructor stub
+    
     }
     
 }
