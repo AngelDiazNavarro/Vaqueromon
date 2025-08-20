3f0576af433740b053dcc5bb9601e345ce0d0cc4
De1cid3 <leonardo.calendario@gmail.com>
2025-08-20 14:59:01 -0400

    incomplete

--- Vaqueromon/src/gopher.java
+++ Vaqueromon/src/gopher.java
@@ -1,10 +1,9 @@
 public class gopher extends mon {
 
-    private final name = gopher ;
 
     public gopher(String name, int health, int atk, int speed, int defense, Mon_type type) {
         super(name, health, atk, speed, defense, type);
-    
+        moves[]
     }
     
 }
