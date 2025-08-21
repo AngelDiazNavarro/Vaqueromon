8d9006ab621b0180c61f563e71d8e7a86b05a79d
De1cid3 <leonardo.calendario@gmail.com>
2025-08-21 14:51:31 -0400

    removed unnecessary file

--- Vaqueromon/src/gopher.java
+++
@@ -1,9 +0,0 @@
-public class gopher extends mon {
-
-
-    public gopher(String name, int health, int atk, int speed, int defense, Mon_type type) {
-        super(name, health, atk, speed, defense, type);
-        moves[]
-    }
-    
-}
